aca8858594ecfec21caef2aaf31dd3a75ec2bb6c
Hicham <53556927+hishamank@users.noreply.github.com>
2021-02-19 00:40:22 +0200

    dark mode

--- styles/theme.js
+++ styles/theme.js
@@ -4,6 +4,7 @@ import { red } from '@material-ui/core/colors';
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
+    type: 'dark',
     primary: {
       main: '#556cd6',
     },
@@ -14,7 +15,7 @@ const theme = createMuiTheme({
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: '#100e0e8c',
     },
   },
 });
